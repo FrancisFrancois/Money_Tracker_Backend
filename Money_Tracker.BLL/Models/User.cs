35e5ec7a922f819ba649e27d2b7c6132d2b9ff0f
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-07 16:50:58 +0100

    Model User et Interface IUserService dans la BLL

---
+++ Money_Tracker.BLL/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Tracker.BLL.Models
+{
+    internal class User
+    {
+    }
+}
