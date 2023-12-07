0257a2707bac7b22398736fc7798fbb0e76f903a
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-07 21:40:10 +0100

    UserDTO + UserMapper

---
+++ Money_Tracker.API/DTOs/UserDTO.cs
@@ -0,0 +1,83 @@
+﻿namespace Money_Tracker.API.DTOs
+{
+    /// <summary>
+    /// Classe représentant un objet de transfert de données (DTO) pour un utilisateur.
+    /// </summary>
+    public class UserDTO
+    {
+        /// <summary>
+        /// ID de l'utilisateur.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nom de l'utilisateur.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Prénom de l'utilisateur.
+        /// </summary>
+        public string Firstname { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Pseudo de l'utilisateur.
+        /// </summary>
+        public string Pseudo { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email de l'utilisateur.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Mot de passe de l'utilisateur.
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Rôles de l'utilisateur.
+        /// </summary>
+        public string Roles { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// Classe représentant un objet de transfert de données (DTO) pour les données d'utilisateur.
+    /// </summary>
+    public class UserDataDTO
+    {
+        /// <summary>
+        /// ID de l'utilisateur.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nom de l'utilisateur.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Prénom de l'utilisateur.
+        /// </summary>
+        public string Firstname { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Pseudo de l'utilisateur.
+        /// </summary>
+        public string Pseudo { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email de l'utilisateur.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Mot de passe de l'utilisateur.
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Rôles de l'utilisateur.
+        /// </summary>
+        public string Roles { get; set; } = string.Empty;
+    }
---
+++ Money_Tracker.API/Mappers/UserMapper.cs
@@ -0,0 +1,49 @@
+﻿using Money_Tracker.API.DTOs;
+using Money_Tracker.BLL.Models;
+
+namespace Money_Tracker.API.Mappers
+{
+    /// <summary>
+    /// Classe statique contenant des méthodes d'extension pour la conversion entre modèles User et UserDTO.
+    /// </summary>
+    public static class UserMapper
+    {
+        /// <summary>
+        /// Convertit un modèle User en UserDTO.
+        /// </summary>
+        /// <param name="model">Modèle User à convertir.</param>
+        /// <returns>UserDTO correspondant au modèle donné.</returns>
+        public static UserDTO ToDTO(this User model)
+        {
+            return new UserDTO
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Firstname = model.Firstname,
+                Pseudo = model.Pseudo,
+                Email = model.Email,
+                Password = model.Password,
+                Roles = model.Roles,
+            };
+        }
+
+        /// <summary>
+        /// Convertit un UserDataDTO en modèle User.
+        /// </summary>
+        /// <param name="user">UserDataDTO à convertir.</param>
+        /// <returns>Modèle User correspondant au UserDataDTO donné.</returns>
+        public static User ToModel(this UserDataDTO user)
+        {
+            return new User
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Firstname = user.Firstname,
+                Pseudo = user.Pseudo,
+                Email = user.Email,
+                Password = user.Password,
+                Roles = user.Roles,
+            };
+        }
+    }
+}
