a9c85bc0361f8a4d9ec2ccf682083742d5041564
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-07 22:57:19 +0100

    suppresion fichier

--- Money_Tracker.DAL/Interfaces/IHomeUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Money_Tracker.DAL.Entities;
-using Money_Tracker.Tools.Interfaces;
-
-namespace Money_Tracker.DAL.Interfaces
-{
-    public interface IHomeUserRepository : ICrud<int, HomeUser>
-    {
-    }
-}
