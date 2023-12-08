d80211c02579a0f4b06aec07385e3acc3a1fa51c
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-08 22:45:31 +0100

    HomeMapper + commentaire

---
+++ Money_Tracker.API/DTOs/HomeDTO.cs
@@ -0,0 +1,65 @@
+﻿namespace Money_Tracker.API.DTOs
+{
+    /// <summary>
+    /// Représente un modèle simplifié pour une maison (Home).
+    /// </summary>
+    public class HomeDTO
+    {
+        /// <summary>
+        /// Obtient ou définit l'identifiant de la maison.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit l'identifiant de l'utilisateur associé à la maison.
+        /// </summary>
+        public int User_Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit le nom de la maison.
+        /// </summary>
+        public string Name_Home { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// Représente un modèle détaillé pour une maison (Home) avec ses utilisateurs associés.
+    /// </summary>
+    public class HomeFullDTO
+    {
+        /// <summary>
+        /// Obtient ou définit l'identifiant de la maison.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit l'identifiant de l'utilisateur associé à la maison.
+        /// </summary>
+        public int User_Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit le nom de la maison.
+        /// </summary>
+        public string Name_Home { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Obtient ou définit une collection de modèles d'utilisateurs associés à la maison.
+        /// </summary>
+        public IEnumerable<HomeUserDTO> Users { get; set; }
+    }
+
+    /// <summary>
+    /// Représente un modèle simplifié pour la relation entre un utilisateur et une maison.
+    /// </summary>
+    public class HomeUserDTO
+    {
+        /// <summary>
+        /// Obtient ou définit l'identifiant de l'utilisateur associé à la maison.
+        /// </summary>
+        public int User_Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit l'identifiant de la maison.
+        /// </summary>
+        public int Home_Id { get; set; }
+    }
+}
---
+++ Money_Tracker.API/Mappers/HomeMapper.cs
@@ -0,0 +1,56 @@
+﻿using Money_Tracker.API.DTOs;
+using Money_Tracker.BLL.Models;
+
+namespace Money_Tracker.API.Mappers
+{
+    /// <summary>
+    /// Classe contenant des méthodes pour mapper les objets Home et HomeUser vers leurs DTO correspondants.
+    /// </summary>
+    public static class HomeMapper
+    {
+        /// <summary>
+        /// Convertit un objet Home en objet HomeDTO.
+        /// </summary>
+        /// <param name="homeModel">Objet Home à mapper.</param>
+        /// <returns>Objet HomeDTO mappé.</returns>
+        public static HomeDTO ToDTO(this Home homeModel)
+        {
+            return new HomeDTO
+            {
+                Id = homeModel.Id,
+                User_Id = homeModel.User_Id,
+                Name_Home = homeModel.Name_Home,
+            };
+        }
+
+        /// <summary>
+        /// Convertit un objet Home en objet HomeFullDTO incluant les utilisateurs associés.
+        /// </summary>
+        /// <param name="homeModel">Objet Home à mapper.</param>
+        /// <returns>Objet HomeFullDTO mappé.</returns>
+        public static HomeFullDTO ToFullDTO(this Home homeModel)
+        {
+            return new HomeFullDTO
+            {
+                Id = homeModel.Id,
+                User_Id = homeModel.User_Id,
+                Name_Home = homeModel.Name_Home,
+                Users = homeModel.Users.Select(u => u.ToHomeUserDTO())
+            };
+        }
+
+        /// <summary>
+        /// Convertit un objet HomeUser en objet HomeUserDTO représentant un utilisateur associé à une maison.
+        /// </summary>
+        /// <param name="homeModel">Objet HomeUser à mapper.</param>
+        /// <returns>Objet HomeUserDTO mappé.</returns>
+        public static HomeUserDTO ToHomeUserDTO(this HomeUser homeModel)
+        {
+            return new HomeUserDTO
+            {
+                User_Id = homeModel.User?.Id ?? 0,
+                Home_Id = homeModel.Home_Id,
+            };
+        }
+    }
+}
