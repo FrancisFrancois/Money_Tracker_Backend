bb051c3b652ed3e3554b0b1bcbf95be9507f854d
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-08 21:14:19 +0100

    HomeUserMapper

---
+++ Money_Tracker.BLL/Models/HomeUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Tracker.BLL.Models
+{
+    /// <summary>
+    /// Représente une relation entre un utilisateur (User) et une maison (Home).
+    /// </summary>
+    public class HomeUser
+    {
+        /// <summary>
+        /// Obtient ou définit l'utilisateur associé à cette relation maison-utilisateur.
+        /// </summary>
+        public User? User { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit la maison associée à cette relation maison-utilisateur.
+        /// </summary>
+        public Home? Home { get; set; }
+    }
+}
