a381b5afb619d4c276bc3f1a710d8dd496e414da
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-08 20:50:06 +0100

    Home Models + IHomeService + HomeMapper

---
+++ Money_Tracker.BLL/Mappers/HomeMapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models = Money_Tracker.BLL.Models;
+using Entities = Money_Tracker.DAL.Entities;
+using System.Runtime.CompilerServices;
+
+namespace Money_Tracker.BLL.Mappers
+{
+    /// <summary>
+    /// Classe statique utilisée pour mapper les objets entre les entités et les modèles Home.
+    /// </summary>
+    public static class HomeMapper
+    {
+        /// <summary>
+        /// Convertit une entité Home en un modèle Home.
+        /// </summary>
+        /// <param name="entity">L'entité Home à convertir.</param>
+        /// <returns>Un objet de type Models.Home.</returns>
+        public static Models.Home ToModel(this Entities.Home entity)
+        {
+            return new Models.Home
+            {
+                Id = entity.Id,
+                User_Id = entity.User_Id,
+                Name_Home = entity.Name_Home,
+            };
+        }
+
+        /// <summary>
+        /// Convertit un modèle Home en une entité Home.
+        /// </summary>
+        /// <param name="model">Le modèle Home à convertir.</param>
+        /// <returns>Un objet de type Entities.Home.</returns>
+        public static Entities.Home ToEntity(this Models.Home model)
+        {
+            return new Entities.Home
+            {
+                Id = model.Id,
+                User_Id = model.User_Id,
+                Name_Home = model.Name_Home,
+            };
+        }
+    }
+}
---
+++ Money_Tracker.BLL/Models/Home.cs
@@ -0,0 +1,31 @@
+﻿using Money_Tracker.DAL.Entities;
+
+
+namespace Money_Tracker.BLL.Models
+{
+    /// <summary>
+    /// Représente une entité "Home" contenant des informations sur une maison.
+    /// </summary>
+    public class Home
+    {
+        /// <summary>
+        /// Obtient ou définit l'identifiant de la maison.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit l'identifiant de l'utilisateur associé à la maison.
+        /// </summary>
+        public int User_Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou définit le nom de la maison.
+        /// </summary>
+        public string Name_Home { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Obtient ou définit une collection d'utilisateurs associés à la maison.
+        /// </summary>
+        public IEnumerable<HomeUser> Users { get; set; }
+    }
+}
