e25e5ef226e600c1e2b361d959c87575dfb9fe5f
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-20 16:58:12 +0100

    Commentaire CategoryService et ExpenseService

--- Money_Tracker.BLL/Services/CategoryService.cs
+++ Money_Tracker.BLL/Services/CategoryService.cs
@@ -3,53 +3,64 @@ using Money_Tracker.BLL.Interfaces;
 using Money_Tracker.BLL.Mappers;
 using Money_Tracker.BLL.Models;
 using Money_Tracker.DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Money_Tracker.BLL.Services
 {
+    // Classe CategoryService : Implémente les opérations de gestion des catégories spécifiées dans l'interface ICategoryService
     public class CategoryService : ICategoryService
     {
+        // Référence au repository de catégories pour l'interaction avec la base de données
         private readonly ICategoryRepository _CategoryRepository;
 
+        // Constructeur pour injecter la dépendance du repository de catégories
         public CategoryService(ICategoryRepository categoryRepository)
         {
             _CategoryRepository = categoryRepository;
         }
 
+        // Récupère toutes les catégories et les convertit en modèles
         public IEnumerable<Category> GetAll()
         {
+            // Utilise le repository pour récupérer toutes les catégories et les convertir en modèles
             return _CategoryRepository.GetAll().Select(c => c.ToModel());
         }
 
+        // Récupère une catégorie spécifique par son ID et la convertit en modèle
         public Category? GetById(int id)
         {
+            // Utilise le repository pour trouver une catégorie par son ID et la convertir en modèle, renvoie null si non trouvée
             return _CategoryRepository.GetById(id)?.ToModel();
         }
 
+        // Crée une nouvelle catégorie
         public Category Create(Category category)
         {
+            // Utilise le repository pour créer une nouvelle catégorie dans la base de données et renvoie le modèle créé
             return _CategoryRepository.Create(category.ToEntity()).ToModel();
         }
 
+        // Met à jour une catégorie existante
         public bool Update(int id, Category category)
         {
+            // Tente de mettre à jour la catégorie et renvoie un booléen indiquant le succès de l'opération.
             bool updated = _CategoryRepository.Update(id, category.ToEntity());
             if (!updated)
             {
+                // Exception indique que l'opération de suppression a échoué 
                 throw new NotFoundException("Category not found");
             }
             return updated;
         }
 
+        // Supprime une catégorie par son ID
         public bool Delete(int id)
         {
+            // Tente de mettre à jour la catégorie et renvoie un booléen indiquant le succès de l'opération.
             bool deleted = _CategoryRepository.Delete(id);
             if (!deleted)
             {
+                // Exception indique que l'opération de suppression a échoué 
                 throw new NotFoundException("Category not found");
             }
             return deleted;
--- Money_Tracker.BLL/Services/ExpenseService.cs
+++ Money_Tracker.BLL/Services/ExpenseService.cs
@@ -1,55 +1,68 @@
-﻿using Money_Tracker.BLL.Interfaces;
+﻿using Money_Tracker.BLL.CustomExceptions;
+using Money_Tracker.BLL.Interfaces;
 using Money_Tracker.BLL.Mappers;
 using Money_Tracker.BLL.Models;
 using Money_Tracker.DAL.Interfaces;
-using Money_Tracker.DAL.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Money_Tracker.BLL.Services
 {
-    public class ExpenseService : IExpenseService   
+    // Classe ExpenseService : Implémente les opérations de gestion des dépenses spécifiées dans l'interface IExpenseService.
+    public class ExpenseService : IExpenseService
     {
+        // Référence au repository des dépenses pour l'interaction avec la base de données.
         private readonly IExpenseRepository _ExpenseRepository;
 
+        // Constructeur pour injecter la dépendance du repository des dépenses.
         public ExpenseService(IExpenseRepository expenseRepository)
         {
             _ExpenseRepository = expenseRepository;
         }
+
+        // Récupère toutes les dépenses et les convertit en modèles (Expense).
         public IEnumerable<Expense> GetAll()
         {
-           return _ExpenseRepository.GetAll().Select(e => e.ToModel());
+            // Utilise le repository pour récupérer toutes les dépenses et les convertit de l'entité vers le modèle Expense.
+            return _ExpenseRepository.GetAll().Select(e => e.ToModel());
         }
 
+        // Récupère une dépense spécifique par son ID et la convertit en modèle Expense.
         public Expense? GetById(int id)
         {
+            // Utilise le repository pour trouver une dépense par son ID. Si trouvée, la convertit en modèle Expense, sinon renvoie null.
             return _ExpenseRepository.GetById(id)?.ToModel();
         }
 
+        // Crée une nouvelle dépense.
         public Expense Create(Expense expense)
         {
+            // Utilise le repository pour créer une nouvelle dépense dans la base de données et renvoie le modèle Expense créé.
             return _ExpenseRepository.Create(expense.ToEntity()).ToModel();
         }
 
+        // Met à jour une dépense existante.
         public bool Update(int id, Expense expense)
         {
+            // Tente de mettre à jour la dépense et renvoie un booléen indiquant le succès de l'opération.
             bool updated = _ExpenseRepository.Update(id, expense.ToEntity());
             if (!updated)
             {
-                throw new DirectoryNotFoundException("Expense not found");
+                // Exception indique que l'opération de suppression a échoué 
+                throw new NotFoundException("Expense not found");
             }
             return updated;
         }
 
+        // Supprime une dépense par son ID.
         public bool Delete(int id)
         {
+            // Tente de supprimer la dépense et renvoie un booléen indiquant le succès de l'opération.
             bool deleted = _ExpenseRepository.Delete(id);
             if (!deleted)
             {
-                throw new DirectoryNotFoundException("Expense not found");
+                // Exception indique que l'opération de suppression a échoué 
+                throw new NotFoundException("Expense not found");
             }
             return deleted;
         }
@@ -137,75 +150,152 @@ namespace Money_Tracker.BLL.Services
 
         #endregion
 
+        // Méthode pour obtenir les dépenses pour un jour spécifique, avec des options de filtrage.
         public IEnumerable<Expense> GetExpensesByDay(DateTime date, int? homeId = null, int? userId = null, int? categoryId = null)
         {
             try
             {
+                // Définit les limites de la journée pour la récupération des dépenses.
+                // La variable 'startDate' est fixée au début de la journée spécifiée par 'date'.
                 DateTime startDate = date.Date;
+
+                // La variable 'endDate' est fixée à la fin de la même journée.
+                // 'AddDays(1)' ajoute un jour à 'startDate', et 'AddSeconds(-1)' soustrait une seconde,
+                // ce qui donne le dernier moment de la journée spécifiée.
                 DateTime endDate = startDate.AddDays(1).AddSeconds(-1);
-                return _ExpenseRepository.GetExpenses(startDate, endDate, homeId, userId, categoryId).Select(e => e.ToModel());
+
+                // Récupère les dépenses pour cette journée en utilisant la méthode 'GetExpenses' du repository
+                // Les dépenses sont récupérées entre 'startDate' et 'endDate'.
+                // Les paramètres optionnels 'homeId', 'userId' et 'categoryId' permettent de filtrer les dépenses
+                // Par domicile, utilisateur ou catégorie, si ces valeurs sont fournies.
+                // Les résultats sont convertis en modèles 'Expense' à l'aide de la méthode 'ToModel'.
+                return _ExpenseRepository.GetExpenses(startDate, endDate, homeId, userId, categoryId).Select(e => e.ToModel()); 
             }
             catch (Exception ex)
             {
+                // En cas d'erreur lors de la récupération des dépenses, une exception est levée.
+                // L'exception originale 'ex' est incluse pour fournir des détails supplémentaires sur l'erreur survenue.
                 throw new Exception("Failed to get expenses by day", ex);
             }
         }
 
+
+        // Méthode pour obtenir les dépenses pour une semaine spécifique, avec des options de filtrage.
         public IEnumerable<Expense> GetExpensesByWeek(DateTime date, int? homeId = null, int? userId = null, int? categoryId = null)
-        {   
+        {
             try
             {
+                // Calcule le début de la semaine basée sur la date donnée.
+                // 'AddDays(-(int)date.DayOfWeek + (int)DayOfWeek.Monday)' calcule le lundi de la semaine actuelle,
+                // En soustrayant le nombre de jours écoulés depuis le lundi.
                 DateTime startDate = date.AddDays(-(int)date.DayOfWeek + (int)DayOfWeek.Monday);
+
+                // Calcule la fin de la semaine (le dimanche suivant).
+                // 'AddDays(7)' ajoute sept jours à 'startDate' (le lundi), puis 'AddSeconds(-1)' soustrait une seconde pour arriver à la fin du dimanche.
                 DateTime endDate = startDate.AddDays(7).AddSeconds(-1);
+
+                // Récupère les dépenses pour cette semaine en utilisant la méthode 'GetExpenses' du repository
+                // Les dépenses sont récupérées pour la semaine définie par 'startDate' et 'endDate'.
+                // Les paramètres optionnels 'homeId', 'userId' et 'categoryId' permettent de filtrer les dépenses
+                // Par domicile, utilisateur ou catégorie, si ces valeurs sont fournies.
+                // Les résultats sont convertis en modèles 'Expense' à l'aide de la méthode 'ToModel'.
                 return _ExpenseRepository.GetExpenses(startDate, endDate, homeId, userId, categoryId).Select(e => e.ToModel());
             }
             catch (Exception ex)
             {
+                // En cas d'erreur lors de la récupération des dépenses, une exception est levée.
+                // L'exception originale 'ex' est incluse pour fournir des détails supplémentaires sur l'erreur survenue.
                 throw new Exception("Failed to get expenses by week", ex);
             }
         }
 
+
+        // Méthode pour obtenir les dépenses pour un mois spécifique, avec des options de filtrage.
         public IEnumerable<Expense> GetExpensesByMonth(DateTime date, int? homeId = null, int? userId = null, int? categoryId = null)
-        {   
+        {
             try
             {
+                // Définit le début du mois basé sur la date donnée.
+                // Utilise l'année et le mois de la date fournie, mais fixe le jour à 1, début du mois.
                 DateTime startDate = new DateTime(date.Year, date.Month, 1);
+
+                // Définit la fin du mois.
+                // 'AddMonths(1)' ajoute un mois à 'startDate' pour se déplacer au début du mois suivant,
+                // puis 'AddDays(-1)' soustrait un jour pour revenir au dernier jour du mois actuel.
                 DateTime endDate = startDate.AddMonths(1).AddDays(-1);
-                return _ExpenseRepository.GetExpenses(startDate, endDate, homeId, userId, categoryId).Select(e => e.ToModel());
+
+                // Récupère les dépenses pour ce mois en utilisant la méthode 'GetExpenses' du repository
+                // Les dépenses sont récupérées pour le mois défini par 'startDate' et 'endDate'.
+                // Les paramètres optionnels 'homeId', 'userId' et 'categoryId' permettent de filtrer les dépenses
+                // Par domicile, utilisateur ou catégorie, si ces valeurs sont fournies.
+                // Les résultats sont convertis en modèles 'Expense' à l'aide de la méthode 'ToModel'.
+                return _ExpenseRepository.GetExpenses(startDate, endDate, homeId, userId, categoryId).Select(e => e.ToModel()); 
             }
             catch (Exception ex)
             {
+                // En cas d'erreur lors de la récupération des dépenses, une exception est levée.
+                // L'exception originale 'ex' est incluse pour fournir des détails supplémentaires sur l'erreur survenue.
                 throw new Exception("Failed to get expenses by month", ex);
             }
         }
 
+
+        // Méthode pour obtenir les dépenses pour une année spécifique, avec des options de filtrage.
         public IEnumerable<Expense> GetExpensesByYear(DateTime date, int? homeId = null, int? userId = null, int? categoryId = null)
-        {   
+        {
             try
             {
+                // Définit le début de l'année basé sur la date donnée.
+                // Utilise l'année de la date fournie, mais fixe le mois et le jour au début de l'année (1er janvier).
                 DateTime startDate = new DateTime(date.Year, 1, 1);
+
+                // Définit la fin de l'année.
+                // Utilise l'année de la date fournie et fixe le mois et le jour à la fin de l'année (31 décembre).
                 DateTime endDate = new DateTime(date.Year, 12, 31);
+
+                // Récupère les dépenses pour cette année en utilisant la méthode 'GetExpenses' du repository
+                // Les dépenses sont récupérées pour l'année définie par 'startDate' et 'endDate'.
+                // Les paramètres optionnels 'homeId', 'userId' et 'categoryId' permettent de filtrer les dépenses
+                // Par domicile, utilisateur ou catégorie, si ces valeurs sont fournies.
+                // Les résultats sont convertis en modèles 'Expense' à l'aide de la méthode 'ToModel'.
                 return _ExpenseRepository.GetExpenses(startDate, endDate, homeId, userId, categoryId).Select(e => e.ToModel());
             }
             catch (Exception ex)
             {
+                // En cas d'erreur lors de la récupération des dépenses, une exception est levée.
+                // L'exception originale 'ex' est incluse pour fournir des détails supplémentaires sur l'erreur survenue.
                 throw new Exception("Failed to get expenses by year", ex);
             }
         }
 
+        // Méthode pour calculer le total des dépenses pour un jour spécifique, avec des options de filtrage.
         public double GetTotalExpensesByDay(DateTime date, int? homeId = null, int? userId = null, int? categoryId = null)
         {
             try
             {
+                // Définit les limites de la journée pour la récupération des dépenses.
+                // La variable 'startDate' est fixée au début de la journée spécifiée par 'date'.
                 DateTime startDate = date.Date;
+
+                // La variable 'endDate' est fixée à la fin de la même journée.
+                // 'AddDays(1)' ajoute un jour à 'startDate', et 'AddSeconds(-1)' soustrait une seconde,
+                // ce qui donne le dernier moment de la journée spécifiée.
                 DateTime endDate = startDate.AddDays(1).AddSeconds(-1);
+
+                // Calcule le total des dépenses pour cette journée en utilisant la méthode 'CalculateTotalExpenses' du repository
+                // Les dépenses sont récupérées pour le jour défini défini par 'startDate' et 'endDate'.
+                // Les paramètres optionnels 'homeId', 'userId' et 'categoryId' permettent de filtrer les dépenses
+                // Par domicile, utilisateur ou catégorie, si ces valeurs sont fournies.
                 return _ExpenseRepository.CalculateTotalExpenses(startDate, endDate, homeId, userId, categoryId);
             }
             catch (Exception ex)
             {
+                // En cas d'erreur lors du calcul du total des dépenses, une exception est levée.
+                // L'exception originale 'ex' est incluse pour fournir des détails supplémentaires sur l'erreur survenue.
                 throw new Exception("Failed to get total expenses by day", ex);
             }
         }
+
 
         public double GetTotalExpensesByWeek(DateTime date, int? homeId = null, int? userId = null, int? categoryId = null)
         {   
