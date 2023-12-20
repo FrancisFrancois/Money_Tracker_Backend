0ee208da80719f5f17790c813da14e2a52ae8f2d
FrancisFrancois <francisfrancois9189@gmai.com>
2023-12-20 09:47:10 +0100

    JwtToken

--- Money_Tracker.API/Controllers/AuthController.cs
+++ Money_Tracker.API/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Money_Tracker.API.DTOs;
 using Money_Tracker.API.Mappers;
 using Money_Tracker.BLL.Interfaces;
-using Money_Tracker.BLL.Services;
+using Money_Tracker.BLL.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Money_Tracker.API.Controllers
 {
@@ -12,10 +15,12 @@ namespace Money_Tracker.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IUserService _UserService;
+        private readonly JwtOptions _JwtOptions;
 
-        public AuthController(IUserService userService)
+        public AuthController(IUserService userService, JwtOptions jwtOptions)
         {
             _UserService = userService;
+            _JwtOptions = jwtOptions;
         }
 
         [HttpPost("Register")]
@@ -38,9 +43,7 @@ namespace Money_Tracker.API.Controllers
             return CreatedAtAction("Register", new { userId = resultDTO.Id }, resultDTO);
         }
 
-        [HttpPost("Login")]
-        [ProducesResponseType(201, Type = typeof(UserDTO))]
-        [ProducesResponseType(400)]
+        [HttpPost("login")]
         public IActionResult Login([FromBody] LoginDTO loginDto)
         {
             if (loginDto is null)
@@ -52,10 +55,44 @@ namespace Money_Tracker.API.Controllers
 
             if (!isValidUser)
             {
-                return Unauthorized("Invalid Credential");
+                return Unauthorized("Invalid credentials");
             }
 
-            return Ok("Login successful");
+            var token = GenerateJwtToken(loginDto.PseudoOrEmail);
+            var expiration = DateTime.Now.AddSeconds(_JwtOptions.Expiration);
+
+            return Ok(new
+            {
+                accessToken = token,
+                Expiration = expiration
+            });
+        }
+
+        private string GenerateJwtToken(string userNameOrEmail)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userNameOrEmail)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_JwtOptions.SigningKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken Token = new JwtSecurityToken
+                (
+
+                issuer: _JwtOptions.Issuer,
+                audience: _JwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddSeconds(_JwtOptions.Expiration),
+                signingCredentials: creds
+
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(Token);
         }
     }
 }
+    
+
+
--- Money_Tracker.API/Program.cs
+++ Money_Tracker.API/Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.IdentityModel.Tokens;
 using Money_Tracker.BLL.Interfaces;
+using Money_Tracker.BLL.Models;
 using Money_Tracker.BLL.Services;
 using Money_Tracker.DAL.Interfaces;
 using Money_Tracker.DAL.Repositories;
 using System.Data.Common;
 using System.Data.SqlClient;
-
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +32,31 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IHomeRepository, HomeRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+
+//JwtToken Config
+JwtOptions options = builder.Configuration.GetSection("JwtOptions").Get<JwtOptions>();
+builder.Services.AddSingleton(options);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer
+    (
+            o =>
+            {
+                //Je vais rechercher ma clé de signature
+                byte[] sKey = Encoding.UTF8.GetBytes(options.SigningKey);
+
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = options.Issuer,
+                    ValidAudience = options.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(sKey)
+                };
+            }
+    );
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
